344d0bb2fa6024cdf234181afc8d9faa5bc69b1c
dea kapanade <medea.kapanadze.1@iliauni.edu.ge>
2021-11-29 18:54:06 +0400

    Add project files.

---
+++ Test_purchee/Models/Stock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public DateTime DateOfOutItems { get; set; }
+        public string UserName { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public int Quantity { get; set; }
+        public int StructureID { get; set; }
+        public string StructureName { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
